30edc48cfa51a4d776085981933f9b77fe068248
Ruiyang Zhou, Desktop <62752455+Fentaniao@users.noreply.github.com>
2025-08-10 22:45:55 +0800

    Update Live Editor font settings
    Set code font to Jetbrains Mono and normal font to Segoe UI, both with size 14px, for improved readability in the Live Editor.

--- MWidgets/LiveScriptCustomize.m
+++ MWidgets/LiveScriptCustomize.m
@@ -1,9 +1,12 @@
 s = settings;
 
-Font_name = "Jetbrains Mono";
 
 % Live Editor code font name
+Font_name = "Jetbrains Mono";
 s.matlab.fonts.editor.code.Name.PersonalValue = Font_name;
+s.matlab.fonts.editor.code.Size.PersonalValue = '14px';
 
 % Live Editor normal font name
+Font_name = "Segoe UI";
 s.matlab.fonts.editor.normal.Name.PersonalValue = Font_name;
+s.matlab.fonts.editor.normal.Size.PersonalValue = '14px';
